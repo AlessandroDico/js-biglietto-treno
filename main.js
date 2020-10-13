07069c8864019735e7303534c50a2bdd9d3e3689
Ale <alessandrochb@gmail.com>
2020-10-13 15:22:32 +0200

    aggiunta condizione per sconto biglietti

--- main.js
+++ main.js
@@ -7,15 +7,42 @@
 
 // prezzo fisso per km € 0.21
 
-var fixed_price = parseFloat(0.21 + '€') ;
+var fixed_price = parseFloat(0.21);
 console.log(fixed_price);
 
-// quanti kilometri percorrerà l'utente.
+// quanti kilometri percorrerà il passeggiero.
 
-var kilometers = parseFloat(prompt('Quanti km percorrerai?'));
+var kilometers = parseFloat(prompt('Quanti km percorrerà il passeggiero?'));
 console.log(kilometers);
 
 // prezzo del biglietto è uguale alla moltiplicazione dei km * prezzo fisso biglietto
 
 var ticket_price = (kilometers * fixed_price);
 console.log(ticket_price);
+
+// ci serve sapere l'età del passeggiero per sapere se può avere scontistiche
+
+var anni_passeggiero = parseInt(prompt('Quanti anni ha il passeggiero?'));
+console.log(anni_passeggiero);
+
+// se il passeggiero è minorenne allora avrà lo sconto del 20%
+// se il passeggiero è over 65 allora avrà lo sconto del 40%
+
+var tiket_price_sale18 = (ticket_price) - ((ticket_price) * 20) /100;
+
+var tiket_price_sale65 = (ticket_price) - ((ticket_price) * 40) /100;
+
+if (anni_passeggiero < 18) {
+    ticket_price = tiket_price_sale18 ;
+    console.log(tiket_price_sale18);
+
+} else if (anni_passeggiero >= 65) {
+    ticket_price = tiket_price_sale65 ;
+    console.log(tiket_price_sale65);
+
+}
+
+
+
+
+/*--------------*/
