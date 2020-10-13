7f3d0e1eb8db5b7fdc61e9e4176d4722b844aa77
Ale <alessandrochb@gmail.com>
2020-10-13 14:46:34 +0200

    aggiunte prime variabili_prezzo fisso_ kilometri_ moltiplicazione

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-document.writeln('hello world');
+// document.writeln('hello world');
+
+// il prezzo del biglietto dipende dalla distanza da percorrere e costa € 0.21 / km
+// è previsto uno sconto del 20% per i minorenni
+// gli over 65 hanno diritto ad uno sconto del 40%
+
+
+// prezzo fisso per km € 0.21
+
+var fixed_price = parseFloat(0.21 + '€') ;
+console.log(fixed_price);
+
+// quanti kilometri percorrerà l'utente.
+
+var kilometers = parseFloat(prompt('Quanti km percorrerai?'));
+console.log(kilometers);
+
+// prezzo del biglietto è uguale alla moltiplicazione dei km * prezzo fisso biglietto
+
+var ticket_price = (kilometers * fixed_price);
+console.log(ticket_price);
