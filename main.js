c31f73cc725c1a8835d3bbb2c27323c7aaf934ce
Ale <alessandrochb@gmail.com>
2020-10-13 16:52:47 +0200

    aggiunti e stilati header e main

--- main.js
+++ main.js
@@ -4,6 +4,13 @@
 // è previsto uno sconto del 20% per i minorenni
 // gli over 65 hanno diritto ad uno sconto del 40%
 
+// richiesta nome e cognome del passeggiero
+var passenger_name = prompt('Inserisci il tuo nome');
+document.getElementById('passenger_name').innerHTML = passenger_name;
+
+var passenger_surname = prompt('Inserisci il tuo cognome');
+document.getElementById('passenger_surname').innerHTML = passenger_surname;
+
 
 // prezzo fisso per km € 0.21
 
@@ -22,8 +29,8 @@ console.log(ticket_price);
 
 // ci serve sapere l'età del passeggiero per sapere se può avere scontistiche
 
-var anni_passeggiero = parseInt(prompt('Quanti anni ha il passeggiero?'));
-console.log(anni_passeggiero);
+var passenger_years = parseInt(prompt('Quanti anni ha il passeggiero?'));
+console.log(passenger_years);
 
 // se il passeggiero è minorenne allora avrà lo sconto del 20%
 // se il passeggiero è over 65 allora avrà lo sconto del 40%
@@ -36,12 +43,12 @@ var final_ticket_price = ticket_price || ticket_price_sale18 || ticket_price_sal
 
 // ISTRUZIONI CONDIZIONALI
 
-if (anni_passeggiero < 18) {
+if (passenger_years < 18) {
     final_ticket_price = ticket_price_sale18 ;
     console.log(ticket_price_sale18);
     document.getElementById('final_price').innerHTML = (ticket_price_sale18 + '€');
 
-} else if (anni_passeggiero >= 65) {
+} else if (passenger_years >= 65) {
     final_ticket_price = ticket_price_sale65 ;
     console.log(ticket_price_sale65);
     document.getElementById('final_price').innerHTML = (ticket_price_sale65 + '€');
